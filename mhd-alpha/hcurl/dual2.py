ab4669972d13b127559544b2f8dac18242a53b92
mingdonghe86 <mingdong.he@queens.ox.ac.uk>
2026-02-24 16:25:09 +0000

    add dual2 scheme, however, not converge

---
+++ mhd-alpha/hcurl/dual2.py
@@ -0,0 +1,362 @@
+# dual2 scheme
+# u2 B2
+# see whether it preserves the three structures
+from firedrake import *
+import csv
+import numpy as np
+from mpi4py import MPI
+import matplotlib.pyplot as plt
+import os
+nu = Constant(0)
+eta = Constant(0)
+S = Constant(1)
+
+def helicity_c(u, B):
+    return assemble(inner(u, B)*dx)
+
+def div_u(u):
+    return norm(div(u), "L2")
+
+def div_B(B):
+    return norm(div(B), "L2")
+
+def energy_uB(u, u_b, B):
+    return 0.5 * assemble(inner(u, u_b) * dx + S * inner(B, B) * dx)
+
+
+# solver parameter
+ICNTL_14 = 5000
+tele_reduc_fac = int(MPI.COMM_WORLD.size/4)
+if tele_reduc_fac < 1:
+    tele_reduc_fac = 1
+
+lu = {
+    "mat_type": "aij",
+    "snes_type": "newtonls",
+    "ksp_type": "preonly",
+    "pc_type": "telescope",
+    "pc_telescope_reduction_factor": tele_reduc_fac,
+    "pc_telescope_subcomm_type": "contiguous",
+    "telescope_pc_type": "lu",
+    "telescope_pc_factor_mat_solver_type": "mumps",
+    "telescope_pc_factor_mat_mumps_icntl_14": ICNTL_14,
+}
+sp = lu
+
+# spatial parameters
+baseN = 4
+nref = 0
+mesh = PeriodicUnitCubeMesh(baseN, baseN, baseN)
+mesh.coordinates.dat.data[:] *= 2 * pi
+
+x, y, z0 = SpatialCoordinate(mesh)
+
+# spatial discretization
+Vg = VectorFunctionSpace(mesh, "CG", 2)
+Q = FunctionSpace(mesh, "CG", 1)
+Vd = FunctionSpace(mesh, "RT", 1)
+Vc = FunctionSpace(mesh, "N1curl", 1)
+Vn = FunctionSpace(mesh, "DG", 0)
+
+# time 
+t = Constant(0) 
+T = 1.0
+dt = Constant(0.01)
+
+alpha = CellDiameter(mesh)
+    
+
+# (u2, P3, w1, u_b2, w_b1, A1, B2, j1)
+Z = MixedFunctionSpace([Vd, Vn, Vc, Vd, Vc, Vc, Vd, Vc])
+z = Function(Z)
+z_test = TestFunction(Z)
+z_prev = Function(Z)
+
+(u, P, w, u_b, w_b, A, B,j) = split(z)
+(ut, Pt, wt, u_bt, w_bt, At, Bt, jt) = split(z_test)
+(up, Pp, wp, u_bp, w_bp, Ap, Bp, jp) = split(z_prev)
+
+# ABC flow
+A0 = Constant(1)
+B0 = Constant(1)
+C0 = Constant(1)
+u1 = A0 * sin(z0) + C0 * cos(y)
+u2 = B0 * sin(x) + A0 * cos(z0)
+u3 = C0 * sin(y) + B0 * cos(x)
+
+u_init = as_vector([u1, u2, u3])
+B_init = as_vector([u1, u2, u3])
+
+alpha = CellDiameter(mesh)
+
+# compute the value of meshsize alpha
+def mesh_sizes(mh):
+     mesh_size = []
+     for msh in mh:
+         DG0 = FunctionSpace(msh, "DG", 0)
+         h = Function(DG0).interpolate(CellDiameter(msh))
+         with h.dat.vec as hvec:
+             _, maxh = hvec.max()
+         mesh_size.append(maxh)
+     return mesh_size
+
+# solve for u_b_init
+def u_b_solver(u):
+    u_init = Function(Vc).interpolate(u)
+    u_b = Function(Vc)
+    v = TestFunction(Vc)
+    F = inner(u_b, v) * dx + alpha**2 * inner(curl(u_b), curl(v)) * dx - inner(u_init, v) * dx
+    sp_ub = {  
+           "ksp_type":"gmres",
+           "pc_type": "ilu",
+    }
+    sp_riesz = {
+         "mat_type": "nest",
+        "snes_type": "ksponly",
+        "snes_monitor": None,
+        "ksp_monitor": None,
+        "ksp_max_it": 1000,
+        "ksp_norm_type": "preconditioned",
+        "ksp_type": "minres",
+        "pc_type": "lu",
+        "pc_factor_mat_solver_type": "mumps",
+        "ksp_atol": 1.0e-5,
+        "ksp_rtol": 1.0e-5,
+        "ksp_minres_nutol": 1E-8,
+        "ksp_convergence_test": "skip",
+
+    }
+    
+    def riesz_u_b(u, v):
+        return inner(u, v) * dx + alpha **2 * inner(curl(u), curl(v)) * dx
+    
+    u_b0 = TrialFunction(Vc)
+    u_b1 = TestFunction(Vc)
+    Jp_riesz = riesz_u_b(u_b0, u_b1)
+
+    bcs0 = [DirichletBC(Vc, 0, "on_boundary")]
+
+    pb0 = NonlinearVariationalProblem(F, u_b, bcs0, Jp = Jp_riesz)
+    solver0 = NonlinearVariationalSolver(pb0, solver_parameters = sp_riesz, options_prefix = "solve curlcurl for u_b") 
+    solver0.solve()
+    return u_b
+
+#u_b_init = u_b_solver(u_init)
+
+def project_ic(B_init):
+    # Need to project the initial conditions
+    # such that div(B) = 0 and B·n = 0
+    Zp = MixedFunctionSpace([Vd, Vn])
+    zp = Function(Zp)
+    (B, p) = split(zp)
+    dirichlet_ids = ("on_boundary",)
+    bcp = [DirichletBC(Zp.sub(0), 0, subdomain) for subdomain in dirichlet_ids]
+    # Write Lagrangian
+    L = (
+          0.5*inner(B, B)*dx
+        - inner(B_init, B)*dx
+        - inner(p, div(B))*dx
+        )
+
+    Fp = derivative(L, zp, TestFunction(Zp))
+    spp = {
+        "mat_type": "nest",
+        "snes_type": "ksponly",
+        "snes_monitor": None,
+        "ksp_monitor": None,
+        "ksp_max_it": 1000,
+        "ksp_norm_type": "preconditioned",
+        "ksp_type": "minres",
+        "pc_type": "fieldsplit",
+        "pc_fieldsplit_type": "additive",
+        "fieldsplit_pc_type": "cholesky",
+        "fieldsplit_pc_factor_mat_solver_type": "mumps",
+        "ksp_atol": 1.0e-5,
+        "ksp_rtol": 1.0e-5,
+        "ksp_minres_nutol": 1E-8,
+        "ksp_convergence_test": "skip",
+    }
+    gamma = Constant(1E5)
+    Up = 0.5*(inner(B, B) + inner(div(B) * gamma, div(B)) + inner(p * (1/gamma), p))*dx
+    Jp = derivative(derivative(Up, zp), zp)
+    solve(Fp == 0, zp, bcp, Jp=Jp, solver_parameters=spp,
+          options_prefix="B_init_div_free_projection")
+ 
+    return zp.subfunctions[0]
+
+
+z_prev.sub(0).interpolate(u_init)
+z_prev.sub(6).interpolate(project_ic(B_init))  # B component
+#z_prev.sub(3).interpolate(u_b_init)
+z.assign(z_prev)
+
+
+u_avg = (u + up)/2
+A_avg = (A + Ap)/2
+B_avg = B
+u_b_avg = u_b
+P_avg = P
+j_avg = j
+w_avg = w
+w_b_avg = w_b
+
+F = (
+    # u
+      inner((u - up)/dt, ut) * dx
+    - inner(cross(u_b_avg, w_avg), ut) * dx # advection term
+    - inner(P_avg, div(ut)) * dx
+    + nu * inner(curl(w_avg), ut) * dx
+    - S * inner(cross(j_avg, B_avg), ut) * dx
+    # p
+    - inner(div(u_avg), Pt) * dx
+    # w  
+    + inner(w_avg, wt) * dx
+    - inner(u_avg, curl(wt)) * dx
+    # u_b
+    + inner(u_b_avg, u_bt) * dx
+    + alpha**2 * inner(curl(w_b_avg), u_bt) * dx
+    - inner(u_avg, u_bt) * dx
+    # w_b
+    + inner(w_b_avg, w_bt) * dx
+    - inner(u_b_avg, curl(w_bt)) * dx
+    # A
+    + inner((A - Ap)/dt, At) * dx
+    + eta * inner(j_avg, At) * dx
+    - inner(cross(u_b_avg, B_avg), At) * dx
+    #B
+    + inner(B_avg, Bt) * dx
+    - inner(curl(A_avg), Bt) * dx
+    #j 
+    + inner(j_avg, jt) * dx
+    - inner(B_avg, curl(jt)) * dx
+)
+
+dirichlet_ids = ("on_boundary",)
+#bcs = [DirichletBC(Z.sub(index), 0, subdomain) for index in range(len(Z)) for subdomain in dirichlet_ids]
+bcs = None
+
+(u_, P_, w_, u_b_, w_b_, A_, B_, j_) = z.subfunctions
+u_.rename("Velocity")
+P_.rename("Pressure")
+u_b_.rename("filteredVelocity")
+w_.rename("Vorticity")
+B_.rename("MagneticField")
+A_.rename("MagneticPotential")
+j_.rename("Current")
+
+pvd = VTKFile("output/mhd-alpha.pvd")
+
+def helicity_m(B):
+    A = Function(Vc)
+    v = TestFunction(Vc)
+    F_curl  = inner(curl(A), curl(v)) * dx - inner(B, curl(v)) * dx
+    sp = {  
+           "ksp_type":"preonly",
+           "pc_type": "lu",
+           "pc_factor_mat_solver_type": "mumps",
+    }
+    bcs_curl = [DirichletBC(Vc, 0, "on_boundary")]
+    pb_curl = NonlinearVariationalProblem(F_curl, A, bcs_curl)
+    solver_curl= NonlinearVariationalSolver(pb_curl, solver_parameters = sp, options_prefix = "solver_curlcurl")
+    solver_curl.solve()
+    return A, assemble(inner(A, B)*dx)
+
+def norm_inf(u):
+    with u.dat.vec_ro as u_v:
+        u_max = u_v.norm(PETSc.NormType.INFINITY)
+    return u_max
+
+def compute_ens(w, j):
+    w_max=norm_inf(w)
+    j_max=norm_inf(j)
+    return w_max, j_max, float(w_max) + float(j_max) 
+
+pb = NonlinearVariationalProblem(F, z)
+solver = NonlinearVariationalSolver(pb, solver_parameters = sp)
+
+timestep = 0
+data_filename = "output/data.csv"
+fieldnames = ["t", "divu", "divB", "energy", "helicity_c", "helicity_m", "ens_total", "w_max", "j_max"]
+# store the mesh info alpha and k_alpha
+if mesh.comm.rank == 0:
+    alpha_val = mesh_sizes(mesh)[0]
+    k_alpha_val = 2 * pi / alpha_val
+
+    with open("output/mesh_info.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["alpha", "k_alpha"])
+        writer.writerow([alpha_val, k_alpha_val])
+
+    print(f"[mesh] alpha = {alpha_val}, k_alpha = {k_alpha_val}")
+if mesh.comm.rank == 0:
+    with open(data_filename, "w", newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+
+#u_b = u_b_solver(z_prev.sub(0))
+energy = energy_uB(z_prev.sub(0), z_prev.sub(3), z_prev.sub(6)) #u, u_b, B
+crosshelicity = helicity_c(z.sub(0), z.sub(6)) # u, B
+A_fn, maghelicity = helicity_m(z.sub(6)) # B
+divu = div_u(z.sub(0))
+divB = div_B(z.sub(6))
+# monitor
+w_max, j_max, ens_total = compute_ens(z.sub(2), z.sub(7)) # w, j
+
+if mesh.comm.rank == 0:
+    row = {
+        "t": float(t),
+        "divu": float(divu),
+        "divB": float(divB),
+        "energy": float(energy),
+        "helicity_c": float(crosshelicity),
+        "helicity_m": float(maghelicity),
+        "ens_total": float(ens_total),
+        "w_max": float(w_max), 
+        "j_max": float(j_max), 
+    }
+    with open(data_filename, "a", newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writerow(row)
+
+while (float(t) < float(T-dt)+1.0e-10):
+    t.assign(t+dt)
+    dofs = Z.dim()
+    dofs_per_core = dofs / COMM_WORLD.size
+    if mesh.comm.rank == 0:
+        print(GREEN % f"Solving for t = {float(t):.4f}, dt = {float(dt)}, T = {T}, baseN = {baseN}, nref = {nref}, nu = {float(nu)}, dofs = {dofs}, dofs_per_core = {dofs_per_core}", flush=True)
+    solver.solve()
+    #u_b = u_b_solver(z.sub(0)) 
+    energy = energy_uB(z.sub(0),z.sub(3), z.sub(6)) #u, u_b, B
+    crosshelicity = helicity_c(z.sub(0), z.sub(6)) # u, u_b, B
+    A_fn, maghelicity = helicity_m(z.sub(6)) # B
+    divu = div_u(z.sub(0))
+    divB = div_B(z.sub(6))
+    # monitor
+    w_max, j_max, ens_tol = compute_ens(z.sub(2), z.sub(7)) # w, j
+
+
+
+    if mesh.comm.rank == 0:
+        row = {
+        "t": float(t),
+        "divu": float(divu),
+        "divB": float(divB),
+        "energy": float(energy),
+        "helicity_c": float(crosshelicity),
+        "helicity_m": float(maghelicity),
+        "ens_total": float(ens_total),
+        "w_max": float(w_max), 
+        "j_max": float(j_max), 
+        }
+        with open(data_filename, "a", newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writerow(row)
+    if mesh.comm.rank == 0:
+        print(row)
+    
+    if mesh.comm.rank == 0:
+        #spectrum_and_save(z.sub(0), A_fn, z.sub(4), float(t))  # 会写 CSV 并存图
+        pvd.write(*z.subfunctions, time=float(t))
+    timestep += 1
+    z_prev.assign(z)
+
